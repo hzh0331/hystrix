33760d0c65e36d19569692b027940e33d6e4a538
hzh <972802149@qq.com>
2020-10-16 13:29:12 +0800

    Initial commit

---
+++ src/main/java/com/example/hystrix/HystrixApplication.java
@@ -0,0 +1,30 @@
+package com.example.hystrix;
+
+import com.example.hystrix.service.TestService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class HystrixApplication {
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(HystrixApplication.class, args);
+        TestService testService = configurableApplicationContext.getBean(TestService.class);
+        for (int i = 0; i < 30; i ++){
+            testService.test1(i < 20 ? 0 : 1);
+            TimeUnit.MILLISECONDS.sleep(300);
+        }
+//        //1
+//        testService.test1(1);
+//        //3
+//        testService.test1(0);
+//        //2
+//        testService.test1(2);
+//        //4
+//        testService.test2(1);
+    }
+}
---
+++ src/main/java/com/example/hystrix/service/TestService.java
@@ -0,0 +1,25 @@
+package com.example.hystrix.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class TestService {
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
+    public void test1(int input){
+        System.out.println("test1 input:"+input);
+        System.out.println("test1 output:"+1/input);
+    }
+
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
+    public void test2(int input) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(1500);
+        System.out.println("test2 "+input);
+    }
+
+    public void fallbackMethod(int input){
+        System.out.println("execute fail, the input is "+input);
+    }
+}
